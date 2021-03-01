b50cf724d1b4610c636f701889a2490bf923323b
uditkandari <udit.kandari@gmail.com>
2021-03-02 00:31:39 +0530

    changed project name

---
+++ SpringMVCHibernateCRUD/src/main/resources/data-mysql.sql
@@ -0,0 +1,6 @@
+create table DataBank(id number auto increment, token varchar2, int weitage);
+insert into DataBank(token, weitage) values ('happy',9);
+insert into DataBank(token, weitage) values ('nice',8);
+insert into DataBank(token, weitage) values ('bad',1);
+insert into DataBank(token, weitage) values ('horrible',1);
+insert into DataBank(token, weitage) values ('good',5);
